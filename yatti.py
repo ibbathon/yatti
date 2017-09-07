222d484102f4b3d306e63ced68fc1191421539a3
Richard David Fears <doctorfears@gmail.com>
2017-09-07 16:45:02 -0400

    Further work to beautify main and standardize
    Added settings for app window size, for saving/restoring between
    sessions, and default size. Changed design so that timer data is all
    stored in a single file, instead of a separate file for each day. Added
    boilerplate code for version update functions. Added update_theme
    function. Rejiggered ordering of widgets, sizing, and how they resize so
    that it looks nicer when it loads.

--- yatti.py
+++ yatti.py
@@ -1,7 +1,7 @@
 """yatti module
 Author = Richard D. Fears
 Created = 2017-08-25
-LastModified = 2017-08-25
+LastModified = 2017-09-07
 Description = Main file of the YATTi program. Builds and displays all of the necessary screens
 	(e.g. timers, calendar, timer data). Also reads and writes out the theme, data, settings,
 	and password files.
@@ -24,14 +24,25 @@ class YattiMain:
 	# DO NOT CHANGE THE DEFAULTS BELOW!
 	# If you want to change your personal theme, edit 'default-theme.json' instead.
 	# If you want to change your settings, edit settings.json.
+	OLDEST_CONVERTIBLE_THEME_VERSION = [1,0,0]
 	DEFAULT_THEME = {
 		'version':[1,0,0],
 		'calendar':{},
 		'timerbuttons':{},
+		'base':{
+			'buttons':{},
+		},
+		'fonts':{
+			'buttons':{'size':20,},
+		},
 	}
+	OLDEST_CONVERTIBLE_SETTINGS_VERSION = [1,0,0]
 	DEFAULT_SETTINGS = {
 		'version':[1,0,0],
+		'root width':1200,
+		'root height':800,
 		'theme file':'default-theme.json',
+		'data file':'timerdata.json',
 		'passwords file':'passwords.bin',
 		'connection info':{
 			'jira':{
@@ -51,10 +62,12 @@ class YattiMain:
 			},
 		},
 	}
+	OLDEST_CONVERTIBLE_DATA_VERSION = [1,0,0]
 	DEFAULT_DATA = {
 		'version':[1,0,0],
 		'timerdata':[],
 	}
+	OLDEST_CONVERTIBLE_PASSWORDS_VERSION = [1,0,0]
 	DEFAULT_PASSWORDS = {
 		'version':[1,0,0],
 		'jira':'',
@@ -63,21 +76,122 @@ class YattiMain:
 	}
 
 	def _settings_version_update (self, oldsettings):
-		pass
+		"""_settings_version_update internal function
+		Updates the old settings to the new format, based on relative versions.
+		"""
+		# Check for versions which are incompatible with list-of-numbers versions
+		try:
+			oldsettings['version'] < [0]
+		except:
+			raise VersionException(VersionException.BAD_TYPE,oldsettings['version'])
+
+		# If the settings version is later than the program version, we will not know how to convert
+		if oldsettings['version'] > YattiMain.DEFAULT_SETTINGS['version']:
+			raise VersionException(VersionException.TOO_NEW,
+				oldsettings['version'],YattiMain.DEFAULT_SETTINGS['version'])
+
+		# If the old version is too old, we will not know how to convert
+		if oldsettings['version'] < YattiMain.OLDEST_CONVERTIBLE_SETTINGS_VERSION:
+			raise VersionException(VersionException.TOO_OLD,
+				oldsettings['version'],YattiMain.DEFAULT_SETTINGS['version'])
+
+		# Finally, convert incrementally through all the versions
+		# Below is some sample code to copy:
+		#if oldsettings['version'] < [1,1]:
+		#	oldsettings['somevariable'] = oldsettings['oldvariable']
+		#	oldsettings['version'] = [1,1]
 
 	def _theme_version_update (self, oldtheme):
-		pass
+		"""_theme_version_update internal function
+		Updates the old theme to the new format, based on relative versions.
+		"""
+		# Check for versions which are incompatible with list-of-numbers versions
+		try:
+			oldtheme['version'] < [0]
+		except:
+			raise VersionException(VersionException.BAD_TYPE,oldtheme['version'])
+
+		# If the theme version is later than the program version, we will not know how to convert
+		if oldtheme['version'] > YattiMain.DEFAULT_THEME['version']:
+			raise VersionException(VersionException.TOO_NEW,
+				oldtheme['version'],YattiMain.DEFAULT_THEME['version'])
+
+		# If the old version is too old, we will not know how to convert
+		if oldtheme['version'] < YattiMain.OLDEST_CONVERTIBLE_THEME_VERSION:
+			raise VersionException(VersionException.TOO_OLD,
+				oldtheme['version'],YattiMain.DEFAULT_THEME['version'])
+
+		# Finally, convert incrementally through all the versions
+		# Below is some sample code to copy:
+		#if oldtheme['version'] < [1,1]:
+		#	oldtheme['somevariable'] = oldtheme['oldvariable']
+		#	oldtheme['version'] = [1,1]
 
 	def _passwords_version_update (self, oldpasswords):
-		pass
+		"""_passwords_version_update internal function
+		Updates the old passwords to the new format, based on relative versions.
+		"""
+		# Check for versions which are incompatible with list-of-numbers versions
+		try:
+			oldpasswords['version'] < [0]
+		except:
+			raise VersionException(VersionException.BAD_TYPE,oldpasswords['version'])
+
+		# If the passwords version is later than the program version,
+		# we will not know how to convert
+		if oldpasswords['version'] > YattiMain.DEFAULT_PASSWORDS['version']:
+			raise VersionException(VersionException.TOO_NEW,
+				oldpasswords['version'],YattiMain.DEFAULT_PASSWORDS['version'])
+
+		# If the old version is too old, we will not know how to convert
+		if oldpasswords['version'] < YattiMain.OLDEST_CONVERTIBLE_PASSWORDS_VERSION:
+			raise VersionException(VersionException.TOO_OLD,
+				oldpasswords['version'],YattiMain.DEFAULT_PASSWORDS['version'])
+
+		# Finally, convert incrementally through all the versions
+		# Below is some sample code to copy:
+		#if oldpasswords['version'] < [1,1]:
+		#	oldpasswords['somevariable'] = oldpasswords['oldvariable']
+		#	oldpasswords['version'] = [1,1]
 
 	def _data_version_update (self, olddata):
-		pass
+		"""_data_version_update internal function
+		Updates the old data to the new format, based on relative versions.
+		"""
+		# Check for versions which are incompatible with list-of-numbers versions
+		try:
+			olddata['version'] < [0]
+		except:
+			raise VersionException(VersionException.BAD_TYPE,olddata['version'])
+
+		# If the data version is later than the program version, we will not know how to convert
+		if olddata['version'] > YattiMain.DEFAULT_DATA['version']:
+			raise VersionException(VersionException.TOO_NEW,
+				olddata['version'],YattiMain.DEFAULT_DATA['version'])
+
+		# If the old version is too old, we will not know how to convert
+		if olddata['version'] < YattiMain.OLDEST_CONVERTIBLE_DATA_VERSION:
+			raise VersionException(VersionException.TOO_OLD,
+				olddata['version'],YattiMain.DEFAULT_DATA['version'])
+
+		# Finally, convert incrementally through all the versions
+		# Below is some sample code to copy:
+		#if olddata['version'] < [1,1]:
+		#	olddata['somevariable'] = olddata['oldvariable']
+		#	olddata['version'] = [1,1]
 
 	def _decrypt_password_file (self, filetext):
+		"""_decrypt_password_file internal function
+		Takes raw text from an encrypted file and converts it to decrypted JSON text string.
+		TODO: Currently just returns the filetext. Need to actually add decryption logic.
+		"""
 		return filetext
 
 	def _encrypt_password_file (self, jsontext):
+		"""_encrypt_password_file internal function
+		Takes a JSON text string and converts it to an encrypted string for writing to a file.
+		TODO: Currently just returns the jsontext. Need to actually add encryption logic.
+		"""
 		return jsontext
 
 	def _load_file_or_defaults (self, dictname, filename, updatefunc, defaults, wrapperfunc = None):
@@ -107,66 +221,120 @@ class YattiMain:
 		yet, it creates base versions.
 		"""
 		# First read in the settings file
-		settingsfilename = "settings.json"
-		self._settings = self._load_file_or_defaults("settings",settingsfilename,
+		self._settingsfilename = "settings.json"
+		self._settings = self._load_file_or_defaults("settings",self._settingsfilename,
 			self._settings_version_update,self.DEFAULT_SETTINGS)
 		# The settings file will contain the rest of the filenames
 		self._theme = self._load_file_or_defaults("theme",self._settings['theme file'],
 			self._theme_version_update,self.DEFAULT_THEME)
+		self._data = self._load_file_or_defaults("data",self._settings['data file'],
+			self._data_version_update,self.DEFAULT_DATA)
 		self._passwords = self._load_file_or_defaults("passwords",self._settings['passwords file'],
 			self._passwords_version_update,self.DEFAULT_PASSWORDS,self._decrypt_password_file)
-		# Build the filename for today's data and try to load it
-		todaysts = time.strftime('%Y%m%d')
-		todaysdatafilename = todaysts+'.json'
-		self._data = self._load_file_or_defaults("data",todaysdatafilename,
-			self._data_version_update,self.DEFAULT_DATA)
+
+	def _canvas_reconfigure (self):
+		"""_canvas_reconfigure callback function
+		Reconfigures the canvas's scrollregion and width when its view is reconfigured.
+		"""
+		# Only reconfigure the canvas if it's already been rendered
+		if not self._canvas.winfo_viewable():
+			self._canvas.after(20,self._canvas_reconfigure)
+			return
+		self._canvas.configure(
+			scrollregion=self._canvas.bbox('all'),
+			width=self._timerframe.winfo_width()
+		)
 
 	def run (self):
 		"""run function
 		Main driver function for YATTi program. Builds all the widgets and runs the main loop.
 		"""
-		root = tk.Tk()
-		root.grid_columnconfigure(1,weight=1,minsize=200)
-		root.grid_columnconfigure(2,weight=3,minsize=400)
-		root.grid_rowconfigure(2,weight=1)
+		self._root = tk.Tk()
+		# Grab the previous size from the settings and place the window in the center of the screen
+		rootwidth = self._settings['root width']
+		rootheight = self._settings['root height']
+		screenwidth = self._root.winfo_screenwidth()
+		screenheight = self._root.winfo_screenheight()
+		xpos = (screenwidth/2)-(rootwidth/2)
+		ypos = (screenheight/2)-(rootheight/2)
+		self._root.geometry('%dx%d+%d+%d'%(rootwidth,rootheight,xpos,ypos))
+		# Column 3 (right pane) and row 2 (timer list) should expand when window resized
+		self._root.grid_columnconfigure(3,weight=1)
+		self._root.grid_rowconfigure(2,weight=1)
 		self._timers = []
 		### Left pane ###
 		# Calendar
-		cal = Calendar(root,self._theme['calendar'])
-		cal.grid(column=1,row=1)
+		self._cal = Calendar(self._root,self._theme['calendar'])
+		self._cal.grid(column=1,row=1)
+		# Add buttons
+		buttonframe = tk.Frame(self._root)
+		buttonframe.grid(column=2,row=1)
+		self._button_font = tkfont.Font()
+		self._add_button = tk.Button(buttonframe,font=self._button_font,text="Add Timer")
+		self._add_button.pack()
+		self._quick_add_button = tk.Button(buttonframe,font=self._button_font,
+			text="Quick-Add Timer")
+		self._quick_add_button.pack()
 		# Timer button frame
-		timerframeouter = tk.Frame(root)
-		timerframeouter.pack_propagate(False)
-		timerframeouter.grid(column=1,row=2,sticky='nsew')
-		canvas = tk.Canvas(timerframeouter)
-		self._timerframe = tk.Frame(canvas)
-		self._timerframe.pack_propagate(False)
-		self._timerframe.pack(fill='both',expand=True)
-		timerscrollbar = tk.Scrollbar(timerframeouter,orient='vertical',command=canvas.yview)
-		canvas.configure(yscrollcommand=timerscrollbar.set)
-		timerscrollbar.pack(side='right',fill='y')
-		canvas.pack(side='left',fill='both',expand=True)
-		canvas.create_window((0,0),window=self._timerframe,anchor='nw')
-		self._timerframe.bind('<Configure>',lambda e, canvas=canvas, \
-			timerframeouter=timerframeouter, timerscrollbar=timerscrollbar: \
-			canvas.configure(scrollregion=canvas.bbox('all'), \
-			width=timerframeouter.winfo_width()-timerscrollbar.winfo_width(),
-			height=timerframeouter.winfo_height()))
-		#self._timerframe.bind('<Configure>',lambda e, canvas=canvas: canvas.pack(side='left',fill='both',expand=True))
+		timerframeouter = tk.Frame(self._root)
+		timerframeouter.grid(column=1,row=2,columnspan=2,sticky='ns')
+		self._canvas = tk.Canvas(timerframeouter)
+		self._timerframe = tk.Frame(self._canvas)
+		self._timerframe.pack()
+		timerscrollbar = tk.Scrollbar(timerframeouter,orient='vertical',command=self._canvas.yview)
+		self._canvas.configure(yscrollcommand=timerscrollbar.set)
+		timerscrollbar.pack(side='right',fill='y',expand=True)
+		self._canvas.pack(side='left',fill='y',expand=True)
+		self._canvas.create_window((0,0),window=self._timerframe,anchor='nw')
+		self._timerframe.bind('<Configure>',lambda e,self=self: self._canvas_reconfigure)
 
+		### Right pane ###
+		templabel = tk.Label(self._root,text="Blah")
+		templabel.grid(row=1,column=3,rowspan=2)
+		#templabel.bind('<Button-1>', lambda e,canvas=self._canvas: canvas.yview_moveto(1))
+
+		### Adding timer buttons and reconfiguring ###
+		self._timerframe.after(5,self.debugadd)
+		self.update_theme()
+
+		self._root.mainloop()
+
+	def update_theme (self):
+		"""update_theme function
+		Updates the fonts/colors/styles from the theme attribute. Used when the user changes
+		the theme.
+		"""
+		widgets = (
+			(self._add_button,'basic'),
+			(self._quick_add_button,'basic'),
+			(self._root,'widget'),
+			(self._add_button,'buttons'),
+			(self._quick_add_button,'buttons'),
+		)
+		fontwidgets = (
+			(self._button_font,'buttons'),
+		)
+		subwidgets = [self._cal]+self._timers
+
+		for widget,name in widgets:
+			helper.configThemeFromDict(widget,self._theme,'base',name)
+		for widget,name in fontwidgets:
+			helper.configThemeFromDict(widget,self._theme,'fonts',name)
+		for widget in subwidgets:
+			widget.update_theme()
+
+
+	def debugadd (self):
+		if not self._timerframe.winfo_viewable():
+			self._timerframe.after(5,self.debugadd)
+			return
 		for i in range(10):
 			self._timers.append(TimerButton(self._timerframe,
 				timertheme=self._theme['timerbuttons']))
-			self._timers[-1].pack()
-		#self._theme['timerbuttons']['base']['widget']['width'] = 100
-		#for i in range(10):
-		#	self._timers[i].update_theme()
-		#self._timerframe.pack_propagate(False)
-		#self._timerframe.configure(width=100,height=100)
-
-		tk.Label(root,text="Blah").grid(row=1,column=2,rowspan=3)
-
-		root.mainloop()
+		for i in range(10):
+			self._timers[i].pack()
+		self._timerframe.update()
+		self._canvas_reconfigure()
 
 
 if __name__ == "__main__":
