20b3e047c88f79160f06670acaba0292b176ea3b
Ibb Marsh <ibbathon@gmail.com>
2019-04-09 20:37:58 -0500

    Moved win32 build options inside "if win32" block

--- setup.py
+++ setup.py
@@ -7,20 +7,19 @@ Description = Compiles the YATTi driver program into an exe.
 import os,sys
 from cx_Freeze import setup, Executable
 
-os.environ['TCL_LIBRARY'] = "C:/Program Files (x86)/Python35-32/tcl/tcl8.6"
-os.environ['TK_LIBRARY'] = "C:/Program Files (x86)/Python35-32/tcl/tk8.6"
+base = None
+build_exe_options = {}
+if sys.platform == "win32":
+	base = "Win32GUI"
 
-build_exe_options = {
-	'include_files':[
+	os.environ['TCL_LIBRARY'] = "C:/Program Files (x86)/Python35-32/tcl/tcl8.6"
+	os.environ['TK_LIBRARY'] = "C:/Program Files (x86)/Python35-32/tcl/tk8.6"
+
+	build_exe_options['include_files'] = [
 		"C:/Program Files (x86)/Python35-32/DLLs/tcl86t.dll",
 		"C:/Program Files (x86)/Python35-32/DLLs/tk86t.dll",
 		"C:/Users/rfears/gitwork/YATTi/YATTi.ico",
 	]
-}
-
-base = None
-if sys.platform == "win32":
-	base = "Win32GUI"
 
 setup(
 	name = "YATTi",
